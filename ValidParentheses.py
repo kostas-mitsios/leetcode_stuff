2a7092cb1471f80b5062a41359296fdf3ac2a41d
Kostas <rotundjere@hotmail.com>
2023-05-17 12:09:36 +0300

    Initial version of ValidParentheses with comments on how to change

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -1,28 +1,24 @@
 
-def isValid(s: str) -> bool:
-        counter = 0
-        valid = True
-        for x in s:
-            if len(s) == 1:
-                return False
-            elif counter%2 != 0:
-                continue
-            match x:
-                case "(":
-                    if s[counter+1] == ")":
-                        valid = True
-                        counter += 1
-                case "[":
-                    if s[counter+1] == "]":
-                        valid = True
-                        counter += 1
-                case "{":
-                    if s[counter+1] == "}":
-                        valid = True
-                        counter += 1
-                case _:
-                    return False
-        return valid
+def checkSubstring(s:str) -> bool:
+    c = 0
+    v = True
+    for s2 in s:
+        #check whole substring for closing symbol
 
+def isValid(s: str) -> bool:
+    #change everything to count how many opening symbols there are and how many closing
+    #if they match, iterate to see if they are in correct order
+    counter = 0
+    valid = True
+    for x in s:
+        if len(s) == 1:
+            return False
+        elif (x == "(") | (x == "[") | (x == "{"):
+            if checkSubstring(s[counter+1:]):
+                counter += 1
+                continue
+            else:
+                return False
+    return valid
 
 print(isValid("()"))
