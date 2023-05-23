e6f4f5bfab8b6aa8250851a4007e72dac4436c0a
Kostas <rotundjere@hotmail.com>
2023-05-23 10:49:38 +0300

    FindIndexOfFirstOccurenceInString solution added

---
+++ Python/FindIndexOfFirstOccurenceInString.py
@@ -0,0 +1,6 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if haystack.find(needle) != -1:
+            return (haystack.find(needle))
+        else:
+            return -1
